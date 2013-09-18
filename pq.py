0bf8bda1ff7c942dd9eeb80e33839056e211923a
eidas <eidabb@yahoo.co.jp>
2013-09-19 00:05:20 +0900

    GitHubにコミットするためライセンスの記述および問題データの形式を整理した。

--- pq.py
+++ pq.py
@@ -4,6 +4,14 @@
 
 #this script is for python 2.x
 
+#petitcom quiz tool
+# quiz data converter for petitcom "quiz 1minute shock".
+
+# Copyright 2013 @eida_s (https://twitter.com/eida_s)
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 import sys
 import codecs
 import struct
@@ -97,3 +105,4 @@ def main():
 if __name__ == '__main__' :
 	main()
 exit()
+
